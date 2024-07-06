9993b0cd9641310fa9d81c0ee03bdf566146cc38
Cucumberchik <durovalexei93@gmail.com>
2024-07-06 16:37:45 +0600

    update route

---
+++ src/components/Me/children/expenses/ExpensesPage.tsx
@@ -0,0 +1,8 @@
+import { ReactNode, FC } from "react";
+import scss from "./expenses.module.scss";
+
+const ExpensesPage: FC = (): ReactNode => {
+  return <></>;
+};
+
+export default ExpensesPage;
